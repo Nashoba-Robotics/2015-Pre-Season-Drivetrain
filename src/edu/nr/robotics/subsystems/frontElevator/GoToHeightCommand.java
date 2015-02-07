bed8bc98796dedfe00feb7baab1f63e51844ecaf
gbear605 <garrisontaylor@gmail.com>
2015-02-07 14:14:31 -0500

    Add go to height command. It is adjustable, and so fills a number of needs.

--- src/edu/nr/robotics/subsystems/frontElevator/GoToHeightCommand.java
+++ src/edu/nr/robotics/subsystems/frontElevator/GoToHeightCommand.java
@@ -1,14 +1,15 @@
 package edu.nr.robotics.subsystems.frontElevator;
 
-import edu.nr.robotics.EmptyCommand;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class AdjustToteOneHeightCommand extends EmptyCommand {
-	
-    public AdjustToteOneHeightCommand() {
+public class GoToHeightCommand extends Command {
+	double height;
+    public GoToHeightCommand(double height) {
         requires(FrontElevator.getInstance());
+        this.height = height;
     }
 
     // Called just before this Command runs the first time
@@ -17,7 +18,7 @@ public class AdjustToteOneHeightCommand extends EmptyCommand {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	FrontElevator.getInstance().setSetpoint(FrontElevator.HEIGHT_ADJUST_TOTE_ONE);
+    	FrontElevator.getInstance().setSetpoint(height);
     }
 
     // Make this return true when this Command no longer needs to run execute()
